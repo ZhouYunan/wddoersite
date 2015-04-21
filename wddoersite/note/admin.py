e4ba951e3f9b3d30b31750b086c43c3964a44d7d
wddoer <wddoer@gmail.com>
2015-04-21 15:53:02 +0800

    admin.py中增加note管理工具

--- wddoersite/note/admin.py
+++ wddoersite/note/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from models import Note, Category
 
 
-admin.site.register(Note)
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'pub_time', 'po_type', )
+    list_display_links = ('title', )
+    list_filter = ('pub_time', 'po_type', )
+    search_fields = ('title', 'po_type', )
+
+
+admin.site.register(Note, NoteAdmin)
 admin.site.register(Category)
