c1c74cc92587cb904b14502e6df690b704e4b2c0
wddoer <wddoer@gmail.com>
2015-04-21 16:04:12 +0800

    增加CategoryAdmin

--- wddoersite/note/admin.py
+++ wddoersite/note/admin.py
@@ -4,11 +4,14 @@ from models import Note, Category
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'pub_time', 'po_type', )
+    list_display = ('title', 'po_type', 'pub_time', )
     list_display_links = ('title', )
     list_filter = ('pub_time', 'po_type', )
-    search_fields = ('title', 'po_type', )
+    search_fields = ('title', )
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_display_links = ('name', )
 
 admin.site.register(Note, NoteAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
