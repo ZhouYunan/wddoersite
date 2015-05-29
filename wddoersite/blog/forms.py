0ed1aab9068ee7d798b62745ffd6b92830cdee90
wddoer <wddoer@gmail.com>
2015-05-29 19:05:05 +0800

    自定义代码高亮方案

--- wddoersite/blog/forms.py
+++ wddoersite/blog/forms.py
@@ -1,10 +1,8 @@
 from models import Note
-from wddoersite.pagedown.widgets import AdminPagedownWidget
 from django import forms
 
 
 class NoteForm(forms.ModelForm):
-    content = forms.CharField(widget=AdminPagedownWidget())
 
     class Meta:
         model = Note
