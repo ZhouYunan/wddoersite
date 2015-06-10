3a0a0f94da409d99e420320bfa3916edfeaf8e3f
wddoer <wddoer@gmail.com>
2015-06-10 13:51:20 +0800

    调用sina财经接口，增加上证和深成实时行情数据

--- wddoersite/stock/urls.py
+++ wddoersite/stock/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'stock/$', views.StockIndexView.as_view(), name="stock_index"),
+    url(r'^$', views.StockIndexView.as_view(), name="stock_index"),
     url(r'real-time/$', views.RealTimeDataView.as_view(), name="real-time"),
 ]
